b637795187b18f878197bf59a99f7fd70a0df8f9
Mirko Rossini <mirkomrossini@gmail.com>
2024-09-23 17:39:00 -0400

    Added picocss

--- svelte-app/src/routes/+page.svelte
+++ svelte-app/src/routes/+page.svelte
@@ -1,21 +1,59 @@
 <script>
-export const prerender = true;
-import {onMount} from 'svelte';
-let promise;
-onMount(async () => {
+	export const prerender = true;
+	import { onMount } from 'svelte';
+	let promise;
+	onMount(async () => {
 		const res = await fetch('http://localhost:1337/app');
-                promise = await res.text();
-});
-
-
+		promise = await res.text();
+	});
 </script>
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-{#await promise}
-<p>...waiting </p>
-{:then ret}
-                <p>Response from the server was: {ret} </p>
-{:catch error}
-	<p style="color: red">{error.message}</p>
-{/await}
+<main class="container">
+	<h1>Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	<details>
+		<summary>Accordion 2</summary>
+		<form>
+			<fieldset  class="grid">
+				<label>
+					First name
+					<input name="first_name" placeholder="First name" autocomplete="given-name" />
+				</label>
+				<label>
+					Email
+					<input type="email" name="email" placeholder="Email" autocomplete="email" />
+				</label>
+			</fieldset>
+
+			<input type="submit" value="Subscribe" />
+		</form>
+	</details>
+        <article>I’m a card!</article>
+        <article>I’m a card!</article>
+        <article>I’m a card!</article>
+        <article>I’m a card!</article>
+        <article>I’m a card!</article>
+        <article>I’m a card!</article>
+        <article>I’m a card!</article>
+        <article>I’m a card!</article>
+        <article>I’m a card!</article>
+        <article>I’m a card!</article>
+        <article>I’m a card!</article>
+        <article>I’m a card!</article>
+        <article>I’m a card!</article>
+        <article>I’m a card!</article>
+        <article>I’m a card!</article>
+        <article>I’m a card!</article>
+        <article>I’m a card!</article>
+        <article>I’m a card!</article>
+
+	{#await promise}
+		<p>...waiting</p>
+	{:then ret}
+		<p>Response from the server was: {ret}</p>
+	{:catch error}
+		<p style="color: red">{error.message}</p>
+	{/await}
+
+	...
+</main>
